09e8d601c88daae5556e066a6a0839898484175d
Ahmet E. Dag <90869456+adag21044@users.noreply.github.com>
2024-09-13 20:00:30 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Counter App', // Dize tırnakları düzeltildi
+      title: 'Counter App', 
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
